a88a08056f11f91c026d1044904f5ea7217bcbce
difbit <36134879+difbit@users.noreply.github.com>
2020-09-21 10:18:39 +0300

    [IMP] Proper field string (#10)

--- sale_order_commitment_date_pivot_and_tree/report/sale_report.py
+++ sale_order_commitment_date_pivot_and_tree/report/sale_report.py
@@ -5,7 +5,7 @@ class SaleReport(models.Model):
 
     _inherit = 'sale.report'
 
-    commitment_date = fields.Datetime(string='Commitment_date', readonly=True)
+    commitment_date = fields.Datetime(string='Commitment Date', readonly=True)
 
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
         fields['commitment_date'] = ", s.commitment_date as commitment_date"

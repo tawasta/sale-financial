abd7708a5b7895eec8e685293f749229ce1adae9
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-09-09 13:24:42 +0300

    [MIG] 14.0 sale_order_down_payment_rounding_difference

---
+++ sale_order_down_payment_rounding_difference/__manifest__.py
@@ -0,0 +1,40 @@
+##############################################################################
+#
+#    Author: Jarmo Kortetjärvi
+#    Copyright 2021 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html.
+#
+##############################################################################
+
+{
+    'name': 'Sale order: Rounding difference line for final invoice when using down payment',
+    'summary': 'Add a rounding difference line for final payment if there is a discrepancy with down payment',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'website': 'https://github.com/Tawasta/sale-financial',
+    'author': 'Tawasta Oy',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'depends': [
+        'sale',
+    ],
+    'data': [
+    ]
+}

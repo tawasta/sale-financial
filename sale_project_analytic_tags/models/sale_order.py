05d1973a3102c60acc1adaa731eecc6c0524259c
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-09-22 14:03:40 +0300

    Modified module to 14 version

---
+++ sale_project_analytic_tags/models/sale_order.py
@@ -0,0 +1,21 @@
+
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    @api.onchange('analytic_account_id')
+    def onchange_analytic_account_id_update_analytic_tags(self):
+        for record in self:
+            if (record.analytic_account_id and
+                    record.analytic_account_id.project_ids
+                    and record.analytic_account_id.project_ids[0].analytic_tag_ids):
+                for line in record.order_line:
+                    if line.display_type:
+                        # Don't set analytic tags for comment lines
+                        continue
+
+                    line.analytic_tag_ids += \
+                        record.analytic_account_id.project_ids[0].analytic_tag_ids
